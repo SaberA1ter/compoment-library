8e4b1eaf494da73786742577d0e32ab8d8ddca9a
SaberAlter <zzqchongya@foxmail.com>
2023-02-14 17:14:17 +0800

    feat: 添加 vitest 测试

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,15 +1,21 @@
+/// <reference types="vitest" />
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
 import VueMacros from 'unplugin-vue-macros/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    // ...
+  },
   plugins: [VueMacros({
     setupComponent: false,
     setupSFC: false,
     plugins: {
       vue: vue(),
+      vueJsx: VueJsx(),
     },
   })],
   build: {
