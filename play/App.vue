35caf707b729aadb3955671659cbaad10c8e5e3f
SaberAlter <zzqchongya@foxmail.com>
2023-08-15 14:53:30 +0800

    feat: 添加 play 工程

---
+++ play/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const handleClick = (event: MouseEvent) => {
+  console.log(event)
+}
+</script>
+
+<template>
+  <h1>组件调试</h1>
+  <mc-button type="primary" @click="handleClick">1234</mc-button>
+</template>
+
+<style scoped>
+
+</style>
